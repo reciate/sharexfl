67bd766970273ff979ab3b024cf69a09eb334cb1
AnusReaper <superzainny@gmail.com>
2020-01-09 13:29:02 +0000

    Added SQLite support

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-import random,string
-from flask import Flask, request, send_from_directory
+import random,string,sqlite3
+from flask import Flask, request, send_from_directory,g
 app = Flask(__name__)
 
 keys = ['test']
 
 def genRandomString(length):
 	return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+	
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('db.db')
+    return db
+	
+def initdb():
+	cursor = get_db().cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS keys(name text NOT NULL, key text NOT NULL)")
+	cursor.execute("CREATE TABLE IF NOT EXISTS uploads(key text NOT NULL, image text NOT NULL)")
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/up', methods=['POST'])
 def upload():
@@ -22,3 +39,6 @@ def upload():
 @app.route('/u/<image>', methods=['GET'])
 def u(image):
 	return send_from_directory('u', image), 200
+	
+with app.app_context():
+	initdb()
