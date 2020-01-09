702d1554e900fbf211a68edeff02c129d9f10273
AnusReaper <49763556+AnusReaper@users.noreply.github.com>
2020-01-09 12:55:34 +0000

    Added error codes and simple key system

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ import random,string
 from flask import Flask, request, send_from_directory
 app = Flask(__name__)
 
+keys = ['test']
+
 def genRandomString(length):
 	return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 @app.route('/up', methods=['POST'])
 def upload():
+	if request.form['key'] not in keys:
+		return 'Your key is invalid!', 401
 	image = request.files['image']
 	filename = '{}.png'.format(genRandomString(6))
 	image.save('u/{}'.format(filename))
-	return '{}u/{}'.format(request.url_root, filename)
+	return '{}u/{}'.format(request.url_root, filename), 200
 	
 @app.route('/u/<image>', methods=['GET'])
 def u(image):
-	return send_from_directory('u', image)
+	return send_from_directory('u', image), 200
