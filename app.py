fce437bff67725589ac8965eaa8251f9ad8c4372
AnusReaper <superzainny@gmail.com>
2020-01-10 09:38:21 +0000

    Switched key system over to SQLite

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import random,string,sqlite3
 from flask import Flask, request, send_from_directory,g
 app = Flask(__name__)
 
-keys = ['test']
-
 def genRandomString(length):
 	return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 	
@@ -14,9 +12,19 @@ def get_db():
     return db
 	
 def initdb():
-	cursor = get_db().cursor()
-	cursor.execute("CREATE TABLE IF NOT EXISTS keys(name text NOT NULL, key text NOT NULL)")
+	db = get_db()
+	cursor = db.cursor()
+	cursor.execute("CREATE TABLE IF NOT EXISTS keys(name text NOT NULL, key text NOT NULL, admin boolean NOT NULL, unique (name, key))")
 	cursor.execute("CREATE TABLE IF NOT EXISTS uploads(key text NOT NULL, image text NOT NULL)")
+	cursor.execute("INSERT OR IGNORE INTO keys (name, key, admin) VALUES (?,?,?)", ('Admin', 'Admin', True))
+	db.commit()
+	
+def keySearch(key):
+	cursor = get_db().cursor()
+	cursor.execute("SELECT * FROM keys WHERE key=?", (key,))
+	if cursor.fetchone() is not None:
+		return True
+	return False
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -27,7 +35,7 @@ def close_connection(exception):
 @app.route('/up', methods=['POST'])
 def upload():
 	try:
-		if request.form['key'] not in keys:
+		if keySearch(request.form['key']) == False:
 			return 'Your key is invalid!', 401
 	except:
 		return 'You must supply a key!', 401
@@ -39,6 +47,13 @@ def upload():
 @app.route('/u/<image>', methods=['GET'])
 def u(image):
 	return send_from_directory('u', image), 200
+	
+@app.route('/db', methods=['GET'])
+def db():
+	cursor = get_db().cursor()
+	cursor.execute("SELECT * FROM keys")
+	data = cursor.fetchall()
+	return ' '.join(str(x) for x in data)
 	
 with app.app_context():
 	initdb()
